86a3cbe4691aa4507efc9526b5cbad20fbfaf62a
Bill Brassfield <bbrassfield@squaretrade.com>
2024-08-31 17:22:11 -0700

    Updated instance name in outputs.tf

--- outputs.tf
+++ outputs.tf
@@ -1,11 +1,11 @@
 output "instance_ami" {
-  value = aws_instance.billtest.ami
+  value = aws_instance.billtest-informatica.ami
 }
 
 output "instance_arn" {
-  value = aws_instance.billtest.arn
+  value = aws_instance.billtest-informatica.arn
 }
 
 output "instance_public_ip" {
-  value = aws_instance.billtest.public_ip
+  value = aws_instance.billtest-informatica.public_ip
 }
