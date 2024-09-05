369f23b9aa7d0aab3484cc57ff952ce258bf897e
Bill Brassfield <bbrassfield@squaretrade.com>
2024-09-05 08:33:01 -0700

    Trying a different subnet and security group to see if VM can reach the Internet

--- main.tf
+++ main.tf
@@ -2,9 +2,9 @@ resource "aws_instance" "billtest-informatica" {
   ami           = "ami-0075013580f6322a1"
   instance_type = var.instance_type
 
-  subnet_id = "subnet-cc8d8597"
+  subnet_id = "subnet-eb38728d"
 
-  vpc_security_group_ids = [aws_security_group.billtest_informatica_sg.id]
+  vpc_security_group_ids = ["sg-99688ce5"]
 
   key_name = "bbrassfield_ubuntu-lab"
 
