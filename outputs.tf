d82afb2ad62180efa005f480101e9433a5de58a2
Bill Brassfield <bbrassfield@squaretrade.com>
2024-10-01 14:10:47 -0700

    Also needed to comment the outputs related to the commented-out EC2 VM 'billtest-informatica'. Added new outputs for the new EC2 VM 'billtest-informatica-2'.

--- outputs.tf
+++ outputs.tf
@@ -1,11 +1,23 @@
-output "instance_ami" {
-  value = aws_instance.billtest-informatica.ami
+# output "instance_ami" {
+#   value = aws_instance.billtest-informatica.ami
+# }
+#
+# output "instance_arn" {
+#   value = aws_instance.billtest-informatica.arn
+# }
+#
+# output "instance_public_ip" {
+#   value = aws_instance.billtest-informatica.public_ip
+# }
+
+output "instance_ami_2" {
+  value = aws_instance.billtest-informatica-2.ami
 }
 
-output "instance_arn" {
-  value = aws_instance.billtest-informatica.arn
+output "instance_arn_2" {
+  value = aws_instance.billtest-informatica-2.arn
 }
 
-output "instance_public_ip" {
-  value = aws_instance.billtest-informatica.public_ip
+output "instance_public_ip_2" {
+  value = aws_instance.billtest-informatica-2.public_ip
 }
