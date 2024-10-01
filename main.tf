baf133f00dcdada2c324213d6aa8f3ba6522a5fd
Bill Brassfield <bbrassfield@squaretrade.com>
2024-10-01 14:06:48 -0700

    Commenting out the original EC2 VM named 'billtest-informatica', so that (hopefully) Terraform will leave this VM as-is (without touching it at all).

--- main.tf
+++ main.tf
@@ -1,38 +1,38 @@
-resource "aws_instance" "billtest-informatica" {
-  ami           = "ami-0075013580f6322a1"
-  instance_type = var.instance_type
-
-  subnet_id = "subnet-eb38728d"
-
-  vpc_security_group_ids = ["sg-99688ce5"]
-
-  key_name = "bbrassfield_ubuntu-lab"
-
-  root_block_device {
-    volume_size = 50
-  }
-
-  tags = {
-    Name = "Bills Terraform Informatica Test VM"
-  }
-
-  user_data = <<-EOF
-              #!/bin/bash
-              wget -O /root/cloud_init_script.sh --no-check-certificate '--user=${var.artifacts_user}' '--password=${var.artifacts_pass}' \
-                https://${var.artifacts_host}${var.artifacts_root}/root/cloud_init_script.sh
-              chmod 700 /root/cloud_init_script.sh
-              IDMC_SA_INSTALLER_USER='${var.idmc_sa_installer_user}' \
-                IDMC_SA_INSTALLER_PASS='${var.idmc_sa_installer_pass}' \
-                IDMC_SA_INSTALLER_GROUP='${var.idmc_sa_installer_group}' \
-                ARTIFACTS_HOST='${var.artifacts_host}' \
-                ARTIFACTS_ROOT='${var.artifacts_root}' \
-                ARTIFACTS_USER='${var.artifacts_user}' \
-                ARTIFACTS_PASS='${var.artifacts_pass}' \
-                VAULT_HOST='${var.vault_host}' \
-                VAULT_TOKEN='${var.vault_token}' \
-                JAS_ENC_PWD='${var.jas_enc_pwd}' /root/cloud_init_script.sh
-              EOF
-}
+# resource "aws_instance" "billtest-informatica" {
+#   ami           = "ami-0075013580f6322a1"
+#   instance_type = var.instance_type
+#
+#   subnet_id = "subnet-eb38728d"
+#
+#   vpc_security_group_ids = ["sg-99688ce5"]
+#
+#   key_name = "bbrassfield_ubuntu-lab"
+#
+#   root_block_device {
+#     volume_size = 50
+#   }
+#
+#   tags = {
+#     Name = "Bills Terraform Informatica Test VM"
+#   }
+#
+#   user_data = <<-EOF
+#               #!/bin/bash
+#               wget -O /root/cloud_init_script.sh --no-check-certificate '--user=${var.artifacts_user}' '--password=${var.artifacts_pass}' \
+#                 https://${var.artifacts_host}${var.artifacts_root}/root/cloud_init_script.sh
+#               chmod 700 /root/cloud_init_script.sh
+#               IDMC_SA_INSTALLER_USER='${var.idmc_sa_installer_user}' \
+#                 IDMC_SA_INSTALLER_PASS='${var.idmc_sa_installer_pass}' \
+#                 IDMC_SA_INSTALLER_GROUP='${var.idmc_sa_installer_group}' \
+#                 ARTIFACTS_HOST='${var.artifacts_host}' \
+#                 ARTIFACTS_ROOT='${var.artifacts_root}' \
+#                 ARTIFACTS_USER='${var.artifacts_user}' \
+#                 ARTIFACTS_PASS='${var.artifacts_pass}' \
+#                 VAULT_HOST='${var.vault_host}' \
+#                 VAULT_TOKEN='${var.vault_token}' \
+#                 JAS_ENC_PWD='${var.jas_enc_pwd}' /root/cloud_init_script.sh
+#               EOF
+# }
 
 resource "aws_instance" "billtest-informatica-2" {
   ami           = "ami-0075013580f6322a1"
