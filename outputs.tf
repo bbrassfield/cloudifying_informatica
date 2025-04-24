e67161d4795acad96816d1ce71bc8fd53201a572
Bill Brassfield <bbrassfield@squaretrade.com>
2025-04-24 14:39:37 -0700

    Removing the first billtest-informatica EC2 instance.

--- outputs.tf
+++ outputs.tf
@@ -1,19 +1,3 @@
-output "instance_ami" {
-  value = aws_instance.billtest-informatica.ami
-}
-
-output "instance_arn" {
-  value = aws_instance.billtest-informatica.arn
-}
-
-output "instance_public_ip" {
-  value = aws_instance.billtest-informatica.public_ip
-}
-
-output "instance_private_ip" {
-  value = aws_instance.billtest-informatica.private_ip
-}
-
 output "instance_ami_2" {
   value = aws_instance.billtest-informatica-2.ami
 }
