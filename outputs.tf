9ba74ae71e91372dc3b160d4ffef121ddf7b8a69
Bill Brassfield <bbrassfield@squaretrade.com>
2025-04-24 12:43:46 -0700

    Added more outputs such as private_ip, and outputs for test vm 4.

--- outputs.tf
+++ outputs.tf
@@ -10,6 +10,10 @@ output "instance_public_ip" {
   value = aws_instance.billtest-informatica.public_ip
 }
 
+output "instance_private_ip" {
+  value = aws_instance.billtest-informatica.private_ip
+}
+
 output "instance_ami_2" {
   value = aws_instance.billtest-informatica-2.ami
 }
@@ -22,6 +26,10 @@ output "instance_public_ip_2" {
   value = aws_instance.billtest-informatica-2.public_ip
 }
 
+output "instance_private_ip_2" {
+  value = aws_instance.billtest-informatica-2.private_ip
+}
+
 output "instance_ami_3" {
   value = aws_instance.billtest-informatica-3.ami
 }
@@ -32,4 +40,24 @@ output "instance_arn_3" {
 
 output "instance_public_ip_3" {
   value = aws_instance.billtest-informatica-3.public_ip
+}
+
+output "instance_private_ip_3" {
+  value = aws_instance.billtest-informatica-3.private_ip
+}
+
+output "instance_ami_4" {
+  value = aws_instance.billtest-informatica-4.ami
+}
+
+output "instance_arn_4" {
+  value = aws_instance.billtest-informatica-4.arn
+}
+
+output "instance_public_ip_4" {
+  value = aws_instance.billtest-informatica-4.public_ip
+}
+
+output "instance_private_ip_4" {
+  value = aws_instance.billtest-informatica-4.private_ip
 }
